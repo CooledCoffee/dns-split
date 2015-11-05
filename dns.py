a48d4e0e84034be5a2ed830c8f0f1c523e8706c0
CooledCoffee <CooledCoffee@gmail.com>
2015-11-05 10:43:02 +0800

    refactor

--- dns.py
+++ dns.py
@@ -18,31 +18,37 @@ sock.bind(('', 53))
 
 cache = {}
 
+def get_from_cache(key, header_id):
+    cached_time, cached_data = cache.get(key, (None, None))
+    if cached_data is None:
+        return None
+    repacked_data = repack(cached_data, header_id, cached_time)
+    if repacked_data is None:
+        del cache[key]
+        return None
+    return repacked_data
+
 def handle_request(data, addr):
     req = DNSRecord.parse(data)
-    qname = str(req.q.qname)
-    key = '%s:%d' % (qname.rstrip('.'), req.questions[0].qtype)
-    result = cache.get(key)
-    if result is not None:
-        cache_time, cached_data = result
-        repacked_data = repack(cached_data, req.header.id, cache_time)
-        if repacked_data is None:
-            del cache[key]
-        else:
-            log.info('Resolved "%s" @cache ...' % key)
-            sock.sendto(repacked_data, addr)
-            return
+    domain = str(req.q.qname).rstrip('.')
+    key = '%s:%d' % (domain, req.questions[0].qtype)
+    resp = get_from_cache(key, req.header.id)
+    if resp is not None:
+        log.info('Resolved "%s" @cache ...' % key)
+        sock.sendto(resp, addr)
+        return
     
     e = event.Event()
     cache[key + '/e'] = e
     start = time.time()
-    dns = decide_dns(qname)
+    dns = decide_dns(domain)
     log.info('Resolving "%s" @%s ...' % (key, dns))
     sock.sendto(data, (dns, 53))
     if e.wait(15):
-        log.info('Resolved "%s" @%s in %d ms.' % (key, dns, (time.time() - start) * 1000))
-        expire, data = cache[key]
-        data = repack(data, req.header.id, expire)
+        millis = (time.time() - start) * 1000
+        log.info('Resolved "%s" @%s in %d ms.' % (key, dns, millis))
+        cached_time, data = cache[key]
+        data = repack(data, req.header.id, cached_time)
         sock.sendto(data, addr)
     else:
         log.info('Failed to resolve "%s" @%s.' % (key, dns))
@@ -76,19 +82,18 @@ def main():
         data, addr = sock.recvfrom(8192)
         gevent.spawn(handler, data, addr)
         
-def repack(data, qid, cache_time):
+def repack(data, qid, cached_time):
     record = DNSRecord.parse(data)
     record.header.id = qid
     now = int(time.time())
     for r in record.rr:
-        ttl = cache_time + r.ttl - now
+        ttl = cached_time + r.ttl - now
         if ttl <= 0:
             return None
         r.ttl = ttl
     return record.pack()
 
-def decide_dns(qname):
-    domain = qname.rstrip('.')
+def decide_dns(domain):
     if 'google' in domain:
         return FOREIGN_DNS
     for d in FOREIGN_DOMAINS:
@@ -102,12 +107,12 @@ def init():
     
     handler = StreamHandler()
     handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(process)d:%(threadName)s] [%(name)s:%(funcName)s:%(lineno)d]\n%(message)s'))
+    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(funcName)s:%(lineno)d]\n%(message)s'))
     log.addHandler(handler)
     
     handler = FileHandler(os.path.join(os.path.dirname(__file__), 'dns.log'))
     handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(process)d:%(threadName)s] [%(name)s:%(funcName)s:%(lineno)d]\n%(message)s'))
+    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(funcName)s:%(lineno)d]\n%(message)s'))
     log.addHandler(handler)
     
 if __name__ == '__main__':
