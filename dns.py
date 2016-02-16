beb0cde07e539b13ea3aeb46f9871e2246331471
CooledCoffee <CooledCoffee@gmail.com>
2016-02-16 10:17:11 +0800

    rewrite dns.py

--- dns.py
+++ dns.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
-from dnslib import DNSRecord
-from gevent import event, socket
+from dnslib.dns import DNSRecord
+from gevent import socket
+from gevent.pool import Pool
 from logging import StreamHandler, FileHandler
-import gevent
 import logging
 import os
 import time
 
-log = logging.getLogger()
-
+BUFFER_SIZE = 8192
+CACHE_EXPIRE_SECONDS = 3600
+DNS_PORT = 53
 DOMESTIC_DNS = '223.5.5.5'
 FOREIGN_DNS = '8.8.8.8'
 FOREIGN_DOMAINS = []
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # @UndefinedVariable
-sock.bind(('', 53))
-
+SOCKET_TIMEOUT = 30
 cache = {}
+log = logging.getLogger()
+sock = None
 
 def calc_key(domain, type):
     key = '%s:%d' % (domain, type)
     return key.lower()
+
+def clean_cache():
+    now = time.time()
+    cnt = 0
+    for key, (cached_time, _) in cache.items():
+        if now - cached_time > CACHE_EXPIRE_SECONDS:
+            del cache[key]
+            cnt += 1
+            log.debug('Removed "%s" from cache.' % key)
+    log.debug('Cleaned %d items from cache.' % cnt)
+
+def decide_dns(domain):
+    if 'google' in domain:
+        return FOREIGN_DNS
+    for d in FOREIGN_DOMAINS:
+        if d == domain or domain.endswith('.' + d):
+            return FOREIGN_DNS
+    else:
+        return DOMESTIC_DNS
 
 def get_from_cache(key, header_id):
     cached_time, cached_data = cache.get(key, (None, None))
@@ -32,60 +52,67 @@ def get_from_cache(key, header_id):
         return None
     return repacked_data
 
-def handle_request(data, addr):
-    req = DNSRecord.parse(data)
-    domain = str(req.q.qname).rstrip('.')
-    key = calc_key(domain, req.questions[0].qtype)
-    resp = get_from_cache(key, req.header.id)
-    if resp is not None:
-        log.info('Resolved "%s" @cache ...' % key)
-        sock.sendto(resp, addr)
-        return
+def handle(req, addr):
+    record = parse(req)
+    key = calc_key(record.q.domain, record.questions[0].qtype)
+    resp = get_from_cache(key, record.header.id)
+    if resp is None:
+        dns = decide_dns(record.q.domain)
+        log.info('Resolving "%s" @%s ...' % (key, dns))
+        out_sock = socket.socket(type=socket.SOCK_DGRAM)  # @UndefinedVariable
+        out_sock.sendto(req, (dns, DNS_PORT))
+        try:
+            socket.wait_read(out_sock.fileno(), SOCKET_TIMEOUT)
+        except:
+            log.warn('Timeout to receive "%s" from "%s".' % (key, dns))
+            return
+        resp = out_sock.recv(BUFFER_SIZE)
+        cache[key] = (int(time.time()), resp)
+    else:
+        dns = 'cache'
+    sock.sendto(resp, addr)
+    log.info('Resolved "%s" @%s.' % (key, dns))
+
+def init():
+    init_logging()
+    init_foreign_domains()
+    init_socks()
     
-    e = event.Event()
-    cache[key + '/e'] = e
-    start = time.time()
-    dns = decide_dns(domain)
-    log.info('Resolving "%s" @%s ...' % (key, dns))
-    sock.sendto(data, (dns, 53))
-    if e.wait(15):
-        millis = (time.time() - start) * 1000
-        log.info('Resolved "%s" @%s in %d ms.' % (key, dns, millis))
-        cached_time, data = cache[key]
-        data = repack(data, req.header.id, cached_time)
-        sock.sendto(data, addr)
-    else:
-        log.info('Failed to resolve "%s" @%s.' % (key, dns))
-
-def handle_response(data):
-    resp = DNSRecord.parse(data)
-    if len(resp.rr) == 0:
-        return
-    domain = str(resp.q.qname).rstrip('.')
-    key = calc_key(domain, resp.questions[0].qtype)
-    e = cache.get(key + '/e')
-    if e is not None:
-        cache[key] = (int(time.time()), data)
-        e.set()
-        del cache[key + '/e']
-
-def handler(data, addr):
-    req = DNSRecord.parse(data)
-    if req.header.qr:
-        handle_response(data)
-    else:
-        handle_request(data, addr)
-
-def main():
+def init_foreign_domains():
     with open(os.path.join(os.path.dirname(__file__), 'foreigns')) as f:
         for line in f:
             line = line.strip()
             if line != '':
                 FOREIGN_DOMAINS.append(line)
-    while True:
-        data, addr = sock.recvfrom(8192)
-        gevent.spawn(handler, data, addr)
-        
+    
+def init_logging():
+    log.setLevel(logging.DEBUG)
+    
+    handler = StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(funcName)s:%(lineno)d]\n%(message)s'))
+    log.addHandler(handler)
+    
+    handler = FileHandler(os.path.join(os.path.dirname(__file__), 'dns.log'))
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(funcName)s:%(lineno)d]\n%(message)s'))
+    log.addHandler(handler)
+    
+def init_socks():
+    global sock
+    sock = socket.socket(type=socket.SOCK_DGRAM)  # @UndefinedVariable
+    sock.bind(('', DNS_PORT))
+    
+def main():
+    init()
+    run()
+    
+def parse(data):
+    record = DNSRecord.parse(data)
+    domain = str(record.q.qname).rstrip('.')
+    record.q.domain = domain
+    return record
+
 def repack(data, qid, cached_time):
     record = DNSRecord.parse(data)
     record.header.id = qid
@@ -97,28 +124,17 @@ def repack(data, qid, cached_time):
         r.ttl = ttl
     return record.pack()
 
-def decide_dns(domain):
-    if 'google' in domain:
-        return FOREIGN_DNS
-    for d in FOREIGN_DOMAINS:
-        if d == domain or domain.endswith('.' + d):
-            return FOREIGN_DNS
-    else:
-        return DOMESTIC_DNS
-    
-def init():
-    log.setLevel(logging.INFO)
-    
-    handler = StreamHandler()
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(funcName)s:%(lineno)d]\n%(message)s'))
-    log.addHandler(handler)
-    
-    handler = FileHandler(os.path.join(os.path.dirname(__file__), 'dns.log'))
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(funcName)s:%(lineno)d]\n%(message)s'))
-    log.addHandler(handler)
+def run():
+    last_clean_time = time.time()
+    pool = Pool(size=8)
+    while True:
+        data, addr = sock.recvfrom(BUFFER_SIZE)
+        pool.spawn(handle, data, addr)
+        if time.time() - last_clean_time > 60:
+            log.debug('Currently %d greenlets.' % len(pool))
+            clean_cache()
+            last_clean_time = time.time()
     
 if __name__ == '__main__':
-    init()
     main()
+    
